6219e7eba4e7f54cf83fcf6213d81ef1626acef6
RishikeshSV <rishuvidyarthi@gmail.com>
2022-02-07 16:34:36 +0530

    q2 done

--- dsa/q2/Source.cpp
+++ dsa/q2/Source.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
+
+void print(char n) {
+	if (isalpha(n)) {
+		int limit = int(n) - 96;
+		cout << "\n";
+		for (int i = 0; i < limit; ++i) {
+			cout << char(97 + i) << " ";
+		}
+	}
+	else {	}
+}
+
 int main() {
-	cout << "HEY";
+	queue<char> letters;
+	char n;
+	do {
+		cout << "\nEnter(a-z):";
+		cin >> n;
+		letters.push(n);
+	} while (isalpha(n));
+	while (!letters.empty()) {
+		char temp = letters.front();
+		print(temp);
+		letters.pop();
+	}
 }
